37edfb567da801d7dda5e8f6e33040650b3f827d
Ehsan Ali <ehsan27ali@gmail.com>
2020-01-02 00:13:00 -0600

    initializing variables

--- main.java
+++ main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Round {
 
   public static void main(String[] args) {
-  
+    
+    //initialize variables
+    int first, last, quotient;
   
   
     System.out.println("Project terminated.");
